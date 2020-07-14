8e95ed65bd1d1032fca700ef3f9d1f8d57acec00
RenanDresch <renan@medroom.com.br>
2020-07-13 21:17:28 -0300

    V 1.0 (#1)
    * WIP single layer, all frames import
    
    * Cel painting offset fixed
    
    * Layer feature (#2)
    
    * Correct stream positioning
    
    * Layer chunk class
    
    * Layer merging working
    
    * Code cleanup and runtime support (#3)
    
    * Package setup (#4)
    
    * Namespace fix
    
    * Package file
    
    * Moved package file

---
+++ Runtime/Data/AseHeader.cs
@@ -0,0 +1,52 @@
+﻿using AsepriteImporter.Runtime.Enums;
+using System;
+using System.IO;
+
+namespace AsepriteImporter.Runtime.Data
+{
+    public class AseHeader
+    {
+        #region Properties
+
+        public UInt16 MagicNumber { get; }
+        public UInt16 FrameCount { get; }
+        public UInt16 Width { get; }
+        public UInt16 Height { get; }
+        public ColorDepth ColorDepth { get; }
+        public Byte PaletteEntryIndex { get; }
+        public UInt16 Colors { get; }
+
+        #endregion
+
+        public AseHeader(BinaryReader reader)
+        {
+            var fileSize = reader.ReadUInt32();
+
+            MagicNumber = reader.ReadUInt16();
+
+            if (MagicNumber != 0xA5E0)
+            {
+                throw new FormatException("Not an Aseprite file!");
+            }
+
+            FrameCount = reader.ReadUInt16();
+            Width = reader.ReadUInt16();
+            Height = reader.ReadUInt16();
+            ColorDepth = (ColorDepth)reader.ReadUInt16();
+            var flags = reader.ReadUInt32();
+            var deprecatedSpeed = reader.ReadUInt16();
+            reader.ReadUInt32(); //Set to 0
+            reader.ReadUInt32(); //Set to 0 
+            PaletteEntryIndex = reader.ReadByte();
+            reader.ReadBytes(3); //Ignore
+            Colors = reader.ReadUInt16();
+            var pixelWidth = reader.ReadByte();
+            var pixelHeight = reader.ReadByte();
+            var gridXPosition = reader.ReadInt16();
+            var gridYPosition = reader.ReadInt16();
+            var gridWidth = reader.ReadUInt16();
+            var gridHeight = reader.ReadUInt16();
+            reader.ReadBytes(84); //For future
+        }
+    }
+}
