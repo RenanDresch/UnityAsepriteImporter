ebf88b09b24a6f313f33f3b853928bff2e8ca675
RenanDresch <renan@medroom.com.br>
2020-07-13 21:17:28 -0300

    V 1.0 (#1)
    * WIP single layer, all frames import
    
    * Cel painting offset fixed
    
    * Layer feature (#2)
    
    * Correct stream positioning
    
    * Layer chunk class
    
    * Layer merging working
    
    * Code cleanup and runtime support (#3)
    
    * Package setup (#4)
    
    * Namespace fix
    
    * Package file
    
    * Moved package file

---
+++ Assets/Package/AsepriteImporter/Runtime/Enums/LayerFlags.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace AsepriteImporter.Runtime.Enums
+{
+    public enum LayerFlags : UInt16
+    {
+        Undefined = 0,
+        Visible = 1,
+        Editable = 2,
+        LockMovement = 3,
+        Background = 4,
+        PreferLinkedCels = 5,
+        Collapsed = 32,
+        RefLayer = 64
+    }
+}
